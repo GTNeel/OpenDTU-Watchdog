c243028ab4c602779d92478584af8c64d8bfeb44
Grant <40084298+GTNeel@users.noreply.github.com>
2026-09-26 18:01:50 -0500

    Update MqttHandleInverter.cpp

--- src/MqttHandleInverter.cpp
+++ src/MqttHandleInverter.cpp
@@ -222,6 +222,9 @@ void MqttHandleInverterClass::onMqttMessage(Topic t, const espMqttClientTypes::M
     case Topic::LimitNonPersistentRelative:
         // Set inverter limit relative non persistent
         ESP_LOGI(TAG, "Limit Non-Persistent: %.1f %%", payload_val);
+        // --- WatchDog Check 2 lines---
+        last_network_cmd_time = millis(); 
+        watchdog_safe_mode_active = false;
         if (!properties.retain) {
             inv->sendActivePowerControlRequest(payload_val, PowerLimitControlType::RelativNonPersistent);
         } else {
@@ -232,6 +235,9 @@ void MqttHandleInverterClass::onMqttMessage(Topic t, const espMqttClientTypes::M
     case Topic::LimitNonPersistentAbsolute:
         // Set inverter limit absolute non persistent
         ESP_LOGI(TAG, "Limit Non-Persistent: %.1f W", payload_val);
+        // --- WatchDog Check 2 lines---
+        last_network_cmd_time = millis(); 
+        watchdog_safe_mode_active = false; 
         if (!properties.retain) {
             inv->sendActivePowerControlRequest(payload_val, PowerLimitControlType::AbsolutNonPersistent);
         } else {
